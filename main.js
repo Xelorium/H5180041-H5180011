3c692256d6876a30e63215080ffcd1c09bec21bd
furkan1yildiz <45845067+furkan1yildiz@users.noreply.github.com>
2019-01-18 20:52:02 +0300

    ilan ver ,giris yap yapıldı, aramaya yeni özellikler eklendi

--- main.js
+++ main.js
@@ -1,16 +1,34 @@
 function basla(){
     $('#kayip').hide();
-}
-var sayac=0;
-function ara(){
-    if(sayac==0){
-    $('#kayip').show();
-sayac++;
-}
-else if(sayac==1){
-    $('#kayip').hide();
-    sayac--;
+    $('#kayip2').hide();
 
+    $('.kayit').hide();
 }
+var sayacara=0;
+var sayacgiris=0;
+function ara(){
+    if(sayacara==0){
+    $('#kayip').show();
+    $('#kayip2').show();
+
+
+sayacara++;
+}
+else if(sayacara==1){
+    $('#kayip').hide();
+    $('#kayip2').hide();
+
+    sayacara--;
+}
+}
+function giris(){
+    if(sayacgiris==0){
+        $('.kayit').show();
+    sayacgiris++;
+    }
+    else if(sayacgiris==1){
+        $('.kayit').hide();
+        sayacgiris--;
+    }
 
 }
