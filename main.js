5cff6e39c9b95daf1fbc79a88f26c1bb1f2166e4
furkan1yildiz <45845067+furkan1yildiz@users.noreply.github.com>
2019-01-18 18:49:39 +0300

    jquery ve buton

--- main.js
+++ main.js
@@ -1,2 +1,20 @@
-
+    sayac=0;
+    function basla(){
+        $('#kayip').hide();
+    
+    }
+    function ara(){
+      
+        if(sayac==0){
+       $('#kayip').show();
+       sayac++;
+    }
+      else if(sayac==1){
+          $('#kayip').hide();
+          sayac--;
+      }
+    }
+    
+    
+    
     
