20222516c6e06ccfd8bb8dbbb215bf750f2ab4b7
furkan1yildiz <45845067+furkan1yildiz@users.noreply.github.com>
2019-01-18 19:32:44 +0300

    hata vardi giderdim

--- main.js
+++ main.js
@@ -1,20 +1,16 @@
-    sayac=0;
-    function basla(){
-        $('#kayip').hide();
-    
-    }
-    function ara(){
-      
-        if(sayac==0){
-       $('#kayip').show();
-       sayac++;
-    }
-      else if(sayac==1){
-          $('#kayip').hide();
-          sayac--;
-      }
-    }
-    
-    
-    
-    
+function basla(){
+    $('#kayip').hide();
+}
+var sayac=0;
+function ara(){
+    if(sayac==0){
+    $('#kayip').show();
+sayac++;
+}
+else if(sayac==1){
+    $('#kayip').hide();
+    sayac--;
+
+}
+
+}
