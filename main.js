452c51d39e79ef75a907098a72d0ab0796be3395
furkan1yildiz <45845067+furkan1yildiz@users.noreply.github.com>
2019-01-20 14:43:57 +0300

    katagorilerin bir kısmı dinamik hale getirildi

--- main.js
+++ main.js
@@ -72,7 +72,7 @@ function basla(){
     var myInner4='';
     var myInner5='';
     var myInner6='';
-
+     
     myInner = myInner + '<img class="card-img-top" src="' + kutu[0].resim + '"/>';
     myInner = myInner + '<div class="card-body">';
     myInner = myInner + '<h5 class="card-title">' + kutu[0].baslik + '</h5>';
@@ -132,7 +132,8 @@ function basla(){
 }
 var user=[
     {
-        nick:'furkan',
+        nick:'furkan1yildiz',
+        ad:'furkan',
         soyad:'yıldız',
         sifre:'987asdfg'
     },
@@ -144,21 +145,24 @@ var user=[
     },
     {
         nick:'yasemin',
+        ad:'yasemin',
         soyad:'genç',
         sifre:'987asdfg'
     },
-    {
-        nick:'ibrahim',
+    {   nick:'at',
+        ad:'ibrahim',
         soyad:'ceylan',
         sifre:'987asdfg'
     },
     {
         nick:'osman',
+        ad:'osman',
         soyad:'gedik',
         sifre:'987asdfg'
     },
     {
         nick:'kerim',
+        ad:'kerim',
         soyad:'yaman',
         sifre:'987asdfg'
     }
@@ -229,3 +233,49 @@ function giris(){
     }
 
 }
+function filtre1(){
+    myInner6='';
+    
+    var myelem7=document.getElementById('sil');
+    myInner6 = myInner6 + '<div id="muzik" class="card" style="width: 20rem; height: 704px;"><img class="card-img-top" src="' + kutu[5].resim + '"/>';
+    myInner6 = myInner6 + '<div class="card-body">';
+    myInner6 = myInner6 + '<h5 class="card-title">' + kutu[5].baslik + '</h5>';
+    myInner6 = myInner6 + '<p class="card-text">' + kutu[5].bilgi+ '</p>';
+    myInner6 = myInner6 + '</div>';
+    myInner6 = myInner6 + '<ul class="list-group list-group-flush"><li class="list-group-item">' + kutu[5].isim+ '</li><li class="list-group-item">' + kutu[5].soyisim+ '</li><li class="list-group-item">' + kutu[5].fiyat+ '</li></ul>';
+    myInner6 = myInner6 + '<div class="card-body"><button type="button" class="btn btn-primary">İlana Git</button></div></div>';
+    document.getElementById('sil').innerHTML = myInner6;
+    }
+    function filtre2(){
+        myInner6='';
+        
+        var myelem7=document.getElementById('sil');
+        myInner6 = myInner6 + '<div id="muzik" class="card" style="width: 20rem; height: 704px;"><img class="card-img-top" src="' + kutu[3].resim + '"/>';
+        myInner6 = myInner6 + '<div class="card-body">';
+        myInner6 = myInner6 + '<h5 class="card-title">' + kutu[3].baslik + '</h5>';
+        myInner6 = myInner6 + '<p class="card-text">' + kutu[3].bilgi+ '</p>';
+        myInner6 = myInner6 + '</div>';
+        myInner6 = myInner6 + '<ul class="list-group list-group-flush"><li class="list-group-item">' + kutu[3].isim+ '</li><li class="list-group-item">' + kutu[3].soyisim+ '</li><li class="list-group-item">' + kutu[3].fiyat+ '</li></ul>';
+        myInner6 = myInner6 + '<div class="card-body"><button type="button" class="btn btn-primary">İlana Git</button></div></div>';
+        document.getElementById('sil').innerHTML = myInner6;
+        }
+        function filtre3(){
+            myInner6='';
+            
+            var myelem7=document.getElementById('sil');
+            myInner6 = myInner6 + '<div id="muzik" class="card" style="width: 20rem; height: 704px;"><img class="card-img-top" src="' + kutu[0].resim + '"/>';
+            myInner6 = myInner6 + '<div class="card-body">';
+            myInner6 = myInner6 + '<h5 class="card-title">' + kutu[0].baslik + '</h5>';
+            myInner6 = myInner6 + '<p class="card-text">' + kutu[0].bilgi+ '</p>';
+            myInner6 = myInner6 + '</div>';
+            myInner6 = myInner6 + '<ul class="list-group list-group-flush"><li class="list-group-item">' + kutu[0].isim+ '</li><li class="list-group-item">' + kutu[0].soyisim+ '</li><li class="list-group-item">' + kutu[0].fiyat+ '</li></ul>';
+            myInner6 = myInner6 + '<div class="card-body"><button type="button" class="btn btn-primary">İlana Git</button></div></div>';
+            document.getElementById('sil').innerHTML = myInner6;
+            }
+            function filtre4(){
+                myInner6='';
+                
+                var myelem7=document.getElementById('sil');
+               myInner6=myInner6+'<div class="card"><div class="card-header">HATA</div><div class="card-body"><p class="card-text">ARADIĞINIZ KRİTERDE SONUÇ BULUNAMADI.</p></div>';
+                document.getElementById('sil').innerHTML = myInner6;
+                }
